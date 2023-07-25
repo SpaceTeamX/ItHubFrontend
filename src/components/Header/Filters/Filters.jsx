46a0166781a095c75fedc7bf92e9c252ed5e28db
PavelHiga <katashihiga@mail.ru>
2023-07-25 22:49:34 +0700

    Сверстал поиск

--- src/components/Header/Filters/Filters.jsx
+++ src/components/Header/Filters/Filters.jsx
@@ -15,7 +15,13 @@ const Filters = () => {
                     <p>Новое</p>
                 </div>
             </div>
-            <div className={style.search}>search</div>
+            <div className={style.searchWrapper}>
+                    <input type="text" placeholder='Поиск'/>
+                <div className={style.magnifier}>
+                    <img src="/assets/magnifier.svg" alt="magnifier" />
+                </div>
+
+            </div>
             <div className={style.filter}>filter</div>
         </div>
     )
