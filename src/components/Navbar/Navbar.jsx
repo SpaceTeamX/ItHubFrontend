f9210b919bda49c3a6d02529bde0abed0dffed5b
Timur <timurkabravo@gmail.com>
2023-07-28 00:00:51 +0600

    Fix theme:Убрать кнопку создать пост Fix:Все сделано

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useLocation } from 'react-router-dom';
 import NavBarItem from './NavBarItem/NavBarItem';
 import style from './navbar.module.scss';
 
 const Navbar = () => {
+  const location = useLocation();
+
   const navList = [
     { name: 'Новости', link: '/', img: '/public/assets/Iconly/Light outline/Calendar.svg' },
     { name: 'Статьи', link: '/articles', img: '/public/assets/Iconly/Light outline/Document.svg' },
@@ -9,17 +12,21 @@ const Navbar = () => {
     { name: 'Видео', link: '/video', img: '/public/assets/Video.svg' },
   ];
 
+  const isNewsPage = location.pathname === '/';
+
   return (
     <div className={style.navbar}>
       <div className={style.navList}>
         {navList.map((item, index) => <NavBarItem item={item} index={index} key={index} />)}
       </div>
-      <button className={style.writePostButton}>
-        <div className={style.btn}>
-          <img src="/public/assets/newpost.svg" alt="pen" /> 
-          <p>Написать пост</p>
-        </div>
-      </button>
+      {isNewsPage ? null : (
+          <button className={style.writePostButton}>
+            <div className={style.btn}>
+              <img src="/public/assets/newpost.svg" alt="pen" /> 
+              <p>Написать пост</p>
+            </div>
+          </button>
+      )}
     </div>
   );
 };
