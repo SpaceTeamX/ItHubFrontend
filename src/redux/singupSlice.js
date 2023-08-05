21d4203b6983ccb13b6fb7735ba1fcf8429fb807
PavelHiga <katashihiga@mail.ru>
2023-08-06 00:13:39 +0700

    рабочая регистрация

--- src/redux/singupSlice.js
+++ src/redux/singupSlice.js
@@ -4,11 +4,16 @@ import axios from "axios";
 export const axiosSingUp = createAsyncThunk(
   "singup/axiosSingUpData",
   async (data) => {
-    console.log(data)
-    const response = await axios.post("https://ktotonekt.pythonanywhere.com/api/users/auth/register/", data);
+    console.log(data);
+    const response = await axios.post(
+      "https://ktotonekt.pythonanywhere.com/api/users/auth/register/",
+      data, 
+    //   {auth:}
+    );
     return response.data;
   }
 );
+
 
 const initialState = {
   token: "",
@@ -26,7 +31,6 @@ const singUpSice = createSlice({
   extraReducers: (builder) => {
     builder.addCase(axiosSingUp.pending, (state) => {
       state.status = "loading";
-
     });
 
     builder.addCase(axiosSingUp.fulfilled, (state, action) => {
@@ -42,6 +46,5 @@ const singUpSice = createSlice({
 
 export const { setSingUpData } = singUpSice.actions;
 export default singUpSice.reducer;
-
 
 // При нажатии на войти будет отправка данных loading... и потом если успешно -> окно и редирект в профиль или основную станицу
