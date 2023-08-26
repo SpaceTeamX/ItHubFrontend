b1d0d82a9e62d97ecc6f8af5ebbc23fbd41246f1
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-08-26 22:41:31 +0300

    доделал форму вакансий по-нынешнему макету

--- src/pages/Vacancy/VacancyForm/VacancyForm.jsx
+++ src/pages/Vacancy/VacancyForm/VacancyForm.jsx
@@ -27,38 +27,56 @@ const VacancyForm = () => {
         <h1>Добавить вакансию</h1>
 
         <div className={style.form}>
-          <h2>Название вакансии</h2>
+          <h2 className={style.subtitle}>Название вакансии</h2>
           <input />
         </div>
 
         <div className={style.formWrapper}>
-          
+          <div className={style.section}>
+            <span className={style.subtitle}>Тип занятости</span>
+            <input className={style.sectionMenu} type="text" />
+          </div>
+          <div className={style.section}>
+            <span className={style.subtitle}>Оклад, руб</span>
+            <div>
+              <input className={style.sectionMenu} type="text" />
+              <input className={style.sectionMenu} type="text" />
+            </div>
+          </div>
+          <div className={style.section}>
+            <span className={style.subtitle}>Место работы</span>
+            <input className={style.sectionMenu} type="text" />
+          </div>
+          <div className={style.section}>
+            <span className={style.subtitle}>Город</span>
+            <input className={style.sectionMenu} type="text" />
+          </div>
         </div>
 
         <div className={style.form}>
-          <h2>Требования</h2>
+          <h2 className={style.subtitle}>Требования</h2>
           <input />
         </div>
 
         <div className={style.form}>
-          <h2>Задачи</h2>
+          <h2 className={style.subtitle}>Задачи</h2>
           <input />
         </div>
 
         <div className={style.form}>
-          <h2>Условия</h2>
+          <h2 className={style.subtitle}>Условия</h2>
           <input />
         </div>
 
         <h1>Компания</h1>
         <div className={style.form}>
-          <h2>Название компании</h2>
+          <h2 className={style.subtitle}>Название компании</h2>
           <input />
         </div>
 
         <h1>Контактная информация</h1>
         <div className={style.form}>
-          <h2>Почта для соискателей</h2>
+          <h2 className={style.subtitle}>Почта для соискателей</h2>
           <input />
         </div>
 
